723d2a9aecf50caf53f0ef05880acb8a2e0e050d
Bartu Baş <bartubas99@gmail.com>
2022-11-05 11:53:02 +0300

    project and initial files setup

---
+++ hw1_e2304129.py
@@ -0,0 +1,31 @@
+
+
+def parse_file_ucs(file_name):
+    #TODO
+    return
+
+
+def parse_file_astar(file_name):
+    #TODO
+    return
+
+
+def ucs(graph):
+    #TODO
+    return
+
+def astar(graph):
+    #TODO
+    return
+
+
+def InformedSearch(method_name, problem_file_name):
+    if method_name == 'UCS':
+        graph = parse_file_ucs(problem_file_name)
+        return ucs(graph)
+    elif method_name == 'AStar':
+        graph = parse_file_astar(problem_file_name)
+        return astar(graph)
+    else:
+        print("Unexpected method name: " + method_name)
+
